24735722fa046f6bb097f86393bf16c926dfed9f
gstotts <32727686+gstotts@users.noreply.github.com>
2021-09-13 17:07:30 -0500

    Update resources.go

--- resources.go
+++ resources.go
@@ -9,23 +9,23 @@ import (
 // STRUCTS
 ///////////////////////////////////////////
 type Query struct {
-	scopes               	[]string
-	filters              	[]string
-	offset               	int
-	limit                	int
-	order_by              	string
-	selected_resource_type	string
+	Scopes               []string `json:"scopes"`
+	Filters              []string `json:"filters"`
+	Offset               int      `json:"offset"`
+	Limit                int      `json:"limit"`
+	OrderBy              string   `json:"order_by"`
+	SelectedResourceType string   `json:"selected_resource_type"`
 }
 
 type QueryResult struct {
-	Scopes               map[string]string `json:"scopes"`
-	Filters              []QueryFilter     `json:"filters"`
-	Offset               int               `json:"offset"`
-	OrderBy              string            `json:"order_by"`
-	Counts               map[string]int    `json:"counts"`
-	SelectedResourceType string            `json:"selected_resource_type"`
-	Resources            []Resource        `json:"resources"`
-	SupportedTypes       []string          `json:"supported_resources"`
+	Scopes               []string       `json:"scopes"`
+	Filters              []QueryFilter  `json:"filters"`
+	Offset               int            `json:"offset"`
+	OrderBy              string         `json:"order_by"`
+	Counts               map[string]int `json:"counts"`
+	SelectedResourceType string         `json:"selected_resource_type"`
+	Resources            []Resource     `json:"resources"`
+	SupportedTypes       []string       `json:"supported_resources"`
 }
 
 type QueryFilter struct {
@@ -43,17 +43,25 @@ type Resource struct {
 
 // QUERY FUNCTIONS
 ///////////////////////////////////////////
-func (c Client) Query(q Query) (map[string]int, error) {
+func (c Client) Query(q *Query) (map[string]int, error) {
+	if q.Filters == nil {
+		q.Filters = make([]string, 0)
+	}
+	if q.Scopes == nil {
+		q.Scopes = make([]string, 0)
+	}
+
 	data, err := json.Marshal(q)
 	if err != nil {
 		return nil, err
 	}
+
 	resp, err := c.makeRequest(http.MethodPost, "/v2/public/resource/query", bytes.NewBuffer(data))
 	if err != nil {
 		return nil, err
 	}
 
-	var ret QueryResult
+	var ret *QueryResult
 	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
 		return nil, err
 	}
