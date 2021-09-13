450fcfc99a5941508910cd62f1da65601a7aa5f4
gstotts <32727686+gstotts@users.noreply.github.com>
2021-09-13 13:50:16 -0500

    Create resources.go

---
+++ resources.go
@@ -0,0 +1,65 @@
+package insightcloudsec
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+// STRUCTS
+///////////////////////////////////////////
+type Query struct {
+	Scopes               map[string]string
+	Filters              map[string]string
+	Offset               int
+	Limit                int
+	OrderBy              string
+	SelectedResourceType string
+}
+
+type QueryResult struct {
+	Scopes               map[string]string `json:"scopes"`
+	Filters              []QueryFilter     `json:"filters"`
+	Offset               int               `json:"ofset"`
+	OrderBy              string            `json:"order_by"`
+	Counts               map[string]int    `json:"counts"`
+	SelectedResourceType string            `json:"selected_resource_type"`
+	Resources            []Resource        `json:"resources"`
+	SupportedTypes       []string          `json:"supported_resources"`
+}
+
+type QueryFilter struct {
+	Config QueryFilterConfig
+	Name   string
+}
+
+type QueryFilterConfig struct {
+}
+
+type Resource struct {
+	Dependencies map[string]string `json:"dependencies"`
+	Details      map[string]string `json:"details"`
+}
+
+// QUERY FUNCTIONS
+///////////////////////////////////////////
+func (c Client) Query(q Query) (map[string]int, error) {
+	data, err := json.Marshal(q)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.makeRequest(http.MethodPost, "/v2/public/resource/query", bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	var ret QueryResult
+	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
+		return nil, err
+	}
+
+	return ret.Counts, nil
+}
+
+// RESOURCE FUNCTIONS
+///////////////////////////////////////////
