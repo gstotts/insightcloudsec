7103ddaef9bdf7c94819b2968cb6a26e6db59195
Gregory Stottsgit config --get-all <mr.stotts@gmail.com>
2021-11-01 16:23:23 -0500

    Adds List_Authentication_Servers() function

---
+++ authentication_servers.go
@@ -0,0 +1,43 @@
+package insightcloudsec
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// CONSTANTS
+///////////////////////////////////////////
+
+// STRUCTS
+///////////////////////////////////////////
+type Server struct {
+	ID           int    `json:"server_id"`
+	Name         string `json:"server_name"`
+	Host         string `json:"server_host"`
+	Port         int    `json:"server_port"`
+	Secure       int    `json:"secure"`
+	Type         string `json:"server_type"`
+	GlobalScope  bool   `json:"global_scope"`
+	MappedGroups int    `json:"mapped_groups"`
+}
+
+type Servers struct {
+	Servers []Server `json:"servers"`
+}
+
+// AUTH SERVER FUNCTIONS
+///////////////////////////////////////////
+
+func (c Client) List_Authentication_Servers() (Servers, error) {
+	resp, err := c.makeRequest(http.MethodPost, "/v2/prototype/authenticationservers/list", nil)
+	if err != nil {
+		return Servers{}, err
+	}
+
+	var ret Servers
+	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
+		return Servers{}, err
+	}
+
+	return ret, nil
+}
