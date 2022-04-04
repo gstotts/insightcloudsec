81bd2ef31cb5b0e17abb452c33ad34d9769fdde3
Gregory Stottsgit config --get-all <mr.stotts@gmail.com>
2022-04-03 22:58:17 -0500

    Update_Cloud_Badges() functionality

--- badges.go
+++ badges.go
@@ -3,8 +3,12 @@ package insightcloudsec
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+// STRUCTS
+///////////////////////////////////////////
 
 type Badge_Creation_Request struct {
 	Resource_IDs []string `json:"target_resource_ids"`
@@ -17,6 +21,13 @@ type Badge struct {
 	Value string `json:"value"`
 }
 
+type Badges struct {
+	Badges []Badge `json:"badges"`
+}
+
+// FUNCTIONS
+///////////////////////////////////////////
+
 func (c Client) Create_Badge(target_resource_ids []string, badge_data []Badge) error {
 	// Creates a badge for target resource ids of key and value pairings provided in map
 	data, err := json.Marshal(Badge_Creation_Request{
@@ -28,6 +39,21 @@ func (c Client) Create_Badge(target_resource_ids []string, badge_data []Badge) e
 	}
 
 	_, err = c.makeRequest(http.MethodPost, "/v2/public/badges/create", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Client) Update_Cloud_Badges(org_resource_id string, badge_data Badges) error {
+	// Updates cloud badges for given organization but overwrites any existing. USE WITH CAUTION
+	data, err := json.Marshal(badge_data)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.makeRequest(http.MethodPost, fmt.Sprintf("/v2/public/badges/%s/update", org_resource_id), bytes.NewBuffer(data))
 	if err != nil {
 		return err
 	}
