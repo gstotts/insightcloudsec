60774fd83ffab399cbe7e70e687ad2d0c5436352
gstotts <32727686+gstotts@users.noreply.github.com>
2022-04-05 23:27:03 -0500

    Update organizations.go

--- organizations.go
+++ organizations.go
@@ -7,72 +7,82 @@ import (
 	"net/http"
 )
 
-// STRUCTS
-///////////////////////////////////////////
-type Organization struct {
+var _ Organizations = (*orgs)(nil)
+
+type Organizations interface {
+	Create(name string) error
+	Edit_Name(resource_id int, name string) error
+	Delete(resource_id int) error
+	List() ([]organizations, error)
+	Switch(name string) error
+}
+
+type orgs struct {
+	client *Client
+}
+
+type organization struct {
 	Name string `json:"organization_name"`
 }
 
-type Organizations struct {
+type organizations struct {
 	Name string `json:"name"`
 	ID   int    `json:"organization_id"`
 }
 
-// ORGANIZATION CLIENT FUNCTIONS
-///////////////////////////////////////////
-func (c Client) Create_Organization(name string) error {
-	data, err := json.Marshal(Organization{Name: name})
+func (c *orgs) Create(name string) error {
+	data, err := json.Marshal(organization{Name: name})
 	if err != nil {
 		return fmt.Errorf("[-] error marshalling organization")
 	}
-	resp, err := c.makeRequest(http.MethodPost, "/v2/prototype/domain/organization/create", bytes.NewBuffer(data))
+	resp, err := c.client.makeRequest(http.MethodPost, "/v2/prototype/domain/organization/create", bytes.NewBuffer(data))
 	if err != nil || resp.StatusCode != 200 {
 		return err
 	}
 	return nil
 }
 
-func (c Client) Switch_Organization(name string) error {
-	data, err := json.Marshal(Organization{Name: name})
+func (c *orgs) Switch(name string) error {
+	data, err := json.Marshal(organization{Name: name})
 	if err != nil {
 		return fmt.Errorf("[-] error marshalling organization")
 	}
-	resp, err := c.makeRequest(http.MethodPost, "/v2/prototype/domain/switch_organization", bytes.NewBuffer(data))
+	resp, err := c.client.makeRequest(http.MethodPost, "/v2/prototype/domain/switch_organization", bytes.NewBuffer(data))
 	if err != nil || resp.StatusCode != 200 {
 		return err
 	}
 	return nil
 }
 
-func (c Client) Edit_Organization_Name(resource_id int, name string) error {
-	data, err := json.Marshal(Organization{Name: name})
+func (c *orgs) Edit_Name(resource_id int, name string) error {
+	data, err := json.Marshal(organization{Name: name})
 	if err != nil {
 		return fmt.Errorf("[-] error marshalling organization")
 	}
-	resp, err := c.makeRequest(http.MethodPost, fmt.Sprintf("/v2/prototype/domain/organization/divvyorganization:%d/update", resource_id), bytes.NewBuffer(data))
+	resp, err := c.client.makeRequest(http.MethodPost, fmt.Sprintf("/v2/prototype/domain/organization/divvyorganization:%d/update", resource_id), bytes.NewBuffer(data))
 	if err != nil || resp.StatusCode != 200 {
 		return err
 	}
 	return nil
 }
 
-func (c Client) Delete_Organization(resource_id int) error {
-	resp, err := c.makeRequest(http.MethodDelete, fmt.Sprintf("/v2/prototype/domain/organization/divvyorganization:%d/delete", resource_id), nil)
+func (c *orgs) Delete(resource_id int) error {
+	resp, err := c.client.makeRequest(http.MethodDelete, fmt.Sprintf("/v2/prototype/domain/organization/divvyorganization:%d/delete", resource_id), nil)
 	if err != nil || resp.StatusCode != 200 {
 		return err
 	}
 	return nil
 }
 
-func (c Client) List_Organizations() ([]Organizations, error) {
-	resp, err := c.makeRequest(http.MethodGet, "/v2/prototype/domain/organizations/get", nil)
+func (c *orgs) List() ([]organizations, error) {
+	resp, err := c.client.makeRequest(http.MethodGet, "/v2/prototype/domain/organizations/get", nil)
 	if err != nil {
-		return []Organizations{}, err
+		return []organizations{}, err
 	}
 
-	var ret []Organizations
+	var ret []organizations
 	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
-		return []Organizations{}, err
+		return []organizations{}, err
 	}
 	return ret, nil
 }
