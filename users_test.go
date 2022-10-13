6aca39f8c1f2d518c3598f04dea9af31709965f8
gstotts <32727686+gstotts@users.noreply.github.com>
2022-10-12 23:44:05 -0500

    Update users_test.go

--- users_test.go
+++ users_test.go
@@ -341,6 +341,12 @@ func TestUsers_GetUserByUsername(t *testing.T) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, getJSONFile("users/listUsersResponse.json"))
 	})
+	mux.HandleFunc("/v2/prototype/domains/admins/list" func(w http.ResponseWriter, r *http.Request {
+		assert.Equal(t, http.MethodPost, r.Method, "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StastusOK)
+	})
+	
 	user, err := client.Users.GetUserByUsername("bbobb")
 	assert.NoError(t, err)
 	assert.Equal(t, "bbobb", user.Username)
