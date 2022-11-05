1d29d46a4b4b5f7e3948a0f79989b9cc01c52e89
gstotts <32727686+gstotts@users.noreply.github.com>
2022-11-04 19:43:39 -0500

    Update authentication_servers.go

--- authentication_servers.go
+++ authentication_servers.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-var _ AuthenticationServers = (&authServers)(nil)
+var _ AuthenticationServers = (authServers)(nil)
 
 type AuthenticationServers interface {
 	List() (AuthenticationServersList, error)
@@ -33,13 +33,13 @@ type AuthenticationServersList struct {
 func (s authServers) List() (AuthenticationServersList, error) {
 	body, err := s.client.makeRequest(http.MethodPost, "/v2/prototype/authenticationservers/list", nil, nil)
 	if err != nil {
-		return &AuthenticationServersList{}, err
+		return AuthenticationServersList{}, err
 	}
 
 	var ret AuthenticationServersList
 	if err := json.Unmarshal(body, &ret); err != nil {
-		return &AuthenticationServersList{}, err
+		return AuthenticationServersList{}, err
 	}
 
-	return &ret, nil
+	return ret, nil
 }
